b3ce0068fce4cd40150ad375afc358806b1ad6ff
ManOwnFire <b.avg@hotmail.com>
2022-07-30 14:14:28 +0300

    ending ch2- not right error when file as input

--- kilo.c
+++ kilo.c
@@ -1,22 +1,41 @@
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+
+/*** data ***/
+
 // global vars
 struct termios orig_termios;
 
 
-// disables raw mode of terminal after program termination
-void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+/*** terminal ***/
+
+// prints an error message and exits the programm
+void die (const *s){
+    /* perror() comes from <stdio.h>, and exit() comes from <stdlib.h>.
+    * Most C library functions that fail will set the global errno variable to indicate what the error was. 
+    * perror() looks at the global errno variable and prints a descriptive error message for it. 
+    */
+    perror(s);
+    exit(1);
 }
 
+// disables raw mode of terminal after program termination
+void disableRawMode() {
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+        die("tcsetattr");
+    };
+}
 
 // sets terminal attributes so that it runs in raw mode
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -24,12 +43,12 @@ void enableRawMode() {
     
     // 'ECHO' prints each key you type to the terminal
     // 'ICANON' allows us to turn off canonical mode
-    // 'ISIG' disable Ctrl-C, Ctrl-Z and Ctrl-Y (Mac Only) signals 
+    // 'ISIG' disables Ctrl-C, Ctrl-Z and Ctrl-Y (Mac Only) signals 
     // 'IXON' disables Ctrl-s and Ctrl-Q signals
     // 'IEXTEN' disable Ctrl-V and Ctrl-o signals
     // 'ICRNL' fices the Ctrl-M signal that pronted as a new line byte 10, I = input, CR = carriage return and NL 0 new line
     // 'OPOST' turns off all of terminal's output processing
-    /**
+    /*
     * When BRKINT is turned on, a break condition will cause a SIGINT signal to be sent to the program, like pressing Ctrl-C.
     * INPCK enables parity checking, which doesn’t seem to apply to modern terminal emulators.
     * ISTRIP causes the 8th bit of each input byte to be stripped, meaning it will set it to 0. This is probably already turned off.
@@ -40,9 +59,16 @@ void enableRawMode() {
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN| ISIG);  // 'c_lflag' is for local flags 
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    /*
+    * VMIN & VTIME come from termios.h and are indexes into c_cc field which stands for control characters an array of bytes that control various terminal settings
+    */
+    raw.c_cc[VMIN] = 0; // sets min# of bytes of input needed before read can return.
+    raw.c_cc[VTIME] = 1; // sets max wait time before read can return
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+/*** init ***/
 
 int main() {
     enableRawMode();
@@ -50,12 +76,22 @@ int main() {
     char c;
 
     // prints each typed char's ASCII numeric value
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    while (1) {
+
+        char c = '/0';
+        /*
+        * errno and EAGAIN come from <errno.h>.
+        * tcsetattr(), tcgetattr(), and read() all return -1 on failure, and set the errno value to indicate the error.
+        */
+
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+
         if (iscntrl(c)) { // iscntrl() comes from <ctype.h>, it tests whether a character is a control character. 
             printf("%d\r\n", c); // '\r' carriage returns the cursor to the beginning of the line and '\n' moves cursor a line downwards
         } else {
             printf ("%d (%c)\r\n", c, c);
         }
+        if (c == 'q') break;
     }
 
     return 0;
