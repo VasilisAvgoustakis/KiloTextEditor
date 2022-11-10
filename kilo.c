a1116dde21b8f85098865f486b10e961d5331987
ManOwnFire <b.avg@hotmail.com>
2022-11-10 19:36:34 +0100

    UpNext ch4: Rendering Tabs

--- kilo.c
+++ kilo.c
@@ -400,9 +400,17 @@ void editorMoveCursor(int key) {
     switch (key) {
         case ARROW_LEFT:
             if (E.cx != 0) E.cx--;
+            else if (E.cy > 0){
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            } 
             break;
         case ARROW_RIGHT:
             if (row && E.cx < row->size) E.cx++;
+            else if (row && E.cx == row->size){
+                E.cy ++;
+                E.cx = 0;
+            }  
             break;
         case ARROW_UP:
             if ( E.cy != 0) E.cy--;
@@ -410,6 +418,13 @@ void editorMoveCursor(int key) {
         case ARROW_DOWN:
             if (E.cy < E.numrows) E.cy++;
             break;
+    }
+
+    // snap cursor to the end of the line
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen){
+        E.cx = rowlen;
     }
 }
 
